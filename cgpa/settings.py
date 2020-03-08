47226fd350563a9c04167a42424532e4e37a6c1a
i-wizard <njoagwuanidavid@gmail.com>
2020-03-08 14:59:49 +0100

    re-designed the UI

--- cgpa/settings.py
+++ cgpa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+6qqp8ew^&xfuk^ujx(=24^j8+psqo#$a@r*g9x1x$0w1b7z^%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.95', '127.0.0.1']
 
 
 # Application definition
